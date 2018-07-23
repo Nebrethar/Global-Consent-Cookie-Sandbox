0fe5ef8c2a9c0db92a0786ede9193c365e365aa2
Nebrethar <nebrethar@gmail.com>
2018-07-23 10:47:05 -0500

    Changed Deletion Strategy to Modification
    httpOnly flag is a big drawback for anyone trying to delete cookies with js

--- Cookie Web Ex/files/cookie.js
+++ Cookie Web Ex/files/cookie.js
@@ -4,14 +4,6 @@ https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/cookies
 */
 
 document.addEventListener("click", (e) => {
-	/*In the case a cookie is removed*/
-	function onRemoved(cookie) {
-		console.log(`Removed: ${cookie}`);
-	}
-	/*In the case a cookie cannot be removed*/
-	function onError(error) {
-		console.log(`Error removing cookie: ${error}`);
-	}
 	/*Function used to log all cookie 
 	values to console (for now)*/
 	function logCookies(cookies) {
@@ -22,30 +14,44 @@ document.addEventListener("click", (e) => {
     }
 	/*Function that will clear all 
 	cookies with a for loop? (maybe soon)*/
-	function clearCookies(cookies) {
-		console.log(cookies);
-	/****This is something I tried, using what I had above.
-	When "cookie" is logged it returnes all values of the cookie, as expected.
-	"cookie.name" returns only the value of the cookie.
-	It seems like I have valid values****/
-	for (let cookie of cookies) {
+	function rewriteCookieValues(cookies) {
+	var i = 1;
+	for (let cookie of cookies){
 	console.log(cookie);
-	console.log(cookie.name);
-	/*I can't seem to get this call to remove cookies by the name string.*/
-	removing = browser.cookies.remove({
-		name: cookie.name
-	});
-	/*This console.log does not seem to report*/
-	console.log(removing);
-	removing.then(onRemoved, onError);
-    }
+	console.log(cookie.value);
+	cookie.name = cookie.name + " " + i;
+	cookie.value = "HI! I'M A COOKIE!"
+	console.log(cookie.name + ": " + cookie.value);
+	i++;
+	}
 	}
     var getting = browser.cookies.getAll({});
 	/*Function is called based on button pressed*/
 	if (e.target.classList.contains("clear")) {
-	getting.then(clearCookies)
+	getting.then(rewriteCookieValues)
 	}
     else if (e.target.classList.contains("log")) {
 	getting.then(logCookies);
     }
     });
+	
+	/***The graveyard. Code that I no longer use but may have use for later***/
+	/*Saved for later
+	console.log(cookie.name);
+	var cname cookie.name;
+	var removing = browser.cookies.remove({
+	name: cname
+	});
+	removing.then(onRemoved, onError);
+	*/
+	/*Ideas from user672118 and paxdiablo at Stack Overflow**/
+	/**https://stackoverflow.com/questions/5688491/unable-to-delete-cookie-from-javascript**/
+	/*
+		console.log(cookies);
+	for (let cookie of cookies) {
+	console.log(cookie.name);
+	//cookie = cookie.name + '=; Max-Age=-99999999;';
+	console.log(cookie);
+	deleteCookie(cookie.name);
+    }
+	*/
