1eb002685c65e87a293e2d890f9d6682c2a27f5a
Matt Snell <msnell@unomaha.edu>
2018-08-07 11:27:21 -0500

    More Automation on Background Script

--- extension-files/background.js
+++ extension-files/background.js
@@ -1,37 +1,112 @@
 /*
 Information on API used can be found at Mozilla WebExtensions documentation
 https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/cookies 
-*/
-
-//UNCOMMENT THIS SCRIPT TO AUTOMATICALLY DELETE ALL EUCONSENT COOKIES
-
-/*browser.cookies.onChanged.addListener(function(changeInfo) 
+*/ 	var i = 0;
+	function decideRem(cookie)
+	{
+		if (cookie != undefined) 
 		{
-			function allb(cookies)
+			removeGVCC(cookie);
+		}
+		else
+		{
+			console.log("Cookie has been deleted automatically (This is a very good thing)");
+		}
+	}
+	function checkGVCC(isRemove)
+	{
+		function sendGVCC(tabs)
+		{
+			var gettingto = browser.cookies.getAll
+			({
+				name:"euconsent"
+			});
+			gettingto.then(decideRem);
+		}
+		var getActive = browser.tabs.query
+		({
+			active: true,
+			currentWindow: true
+		})
+		getActive.then(sendGVCC);
+	}
+	function onRemoved() 
+	{
+		console.log("Removed!");
+		checkGVCC();
+	}
+	function onError(error) 
+	{
+		console.error(error);
+	}
+	function initiateRemoval()
+	{
+		function success()
+		{
+			console.log("Done!");
+		}					
+		function whynot(error)
+		{
+			console.log(error);
+		}
+		function removeCookies(tabs)
+		{
+			coneole.log("Marker 2");
+			var cookierem = browser.cookies.remove
+			({
+				url: tabs[0].url,
+				name: "euconsent"
+			});
+			cookierem.then(success, whynot);						
+		}	
+		console.log("Marker 1");
+		var getActive = browser.tabs.query
+		({
+			active: true,
+			currentWindow: true
+		});
+		getActive.then(removeCookies);
+	}
+    browser.runtime.onMessage.addListener(initiateRemoval);
+	function check(cookie)
+	{
+		if (cookie.name == "euconsent")
+		{
+			console.log("Found one!");
+				var cookieValue = cookie.value.split('-');
+				var cookiein = "";
+				var cookieout;
+				for (var i=0;i<cookieValue.length;i++)
+				{
+					if (i < cookieValue.length - 2)
 					{
-						//console.log(cookies);
-						if (cookies.length != 0) {
-						console.log("STANDARD GVCC REMOVED");
-						}
+						cookiein = cookiein + cookieValue[i] + "-";
 					}
-					function removeCookies(tabs)
+					else if (i < cookieValue.length - 1)
 					{
-					function didit(){
+						cookiein = cookiein + cookieValue[i];
 					}
-					var allc = browser.cookies.getAll({
-						name:"euconsent"
-					});
-					var cookierem = browser.cookies.remove({
-						//Works on one site. Still have to figure out domain issues.
-						url: tabs[0].url,
-						name: "euconsent"
-					});
-					allc.then(allb);
-					cookierem.then();
+					else
+					{
+						cookieout = cookieValue[i];
 					}
-					var getActive = browser.tabs.query({
-					active: true,
-					currentWindow: true
+					if (cookiein != "BOSCllVOSCllVABABBENBZAAAAAfaAAA")
+					{
+					console.log("marker 0.5");
+					browser.tabs.executeScript
+					({
+						file: "write-cookie.js"
 					});
-					getActive.then(removeCookies);
-		});*/
+					}
+					else
+					{
+						console.log("Nothing I can do here, captain!");
+					}
+				}
+		}
+	}
+	browser.cookies.onChanged.addListener(function(changeInfo) 
+		{
+			check(changeInfo.cookie);
+		});
+		console.log("ok!");
