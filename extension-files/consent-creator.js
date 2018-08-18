6e0829284877bc759fe9e0b94b4cad032143d717
Matt Snell <msnell@unomaha.edu>
2018-08-17 23:12:11 -0500

    Added site support

--- extension-files/consent-creator.js
+++ extension-files/consent-creator.js
@@ -7,7 +7,9 @@ async function initiateCookie() {
     let tabs = await browser.tabs.query({active: true, currentWindow: true});
     let urlSet = tabs[0].url;
     let nameSet = "euconsent";
-    let valueSet = "No value set!";
+    let valueSet = "BOSl-jdOSl-jlABABBENBd-AAAAgV___________" +
+            "___________________________________________" +
+            "________________________________A";
     let httpOnlySet = false;
     let pathSet = "/";
     let firstPartyDomainSet = "";
@@ -15,6 +17,22 @@ async function initiateCookie() {
     // this seems to work pretty well for every website
     let domain = tabs[0].url.split("/")[2];
     let domainSet = "." + domain;
+    // for debugging
+    //console.log("DOMAIN: " + domain);
+    async function setCookie() {
+        await browser.cookies.set({
+            url: urlSet,
+            name: nameSet,
+            value: valueSet,
+            httpOnly: httpOnlySet,
+            path: pathSet,
+            firstPartyDomain: firstPartyDomainSet,
+            storeId: storeIdSet,
+            domain: domainSet,
+            secure: false,
+            expirationDate: 1566099891,
+        });
+    }
     /*
     __________________________________________________________
     Navigating the publisher consent string switch stsatment
@@ -31,30 +49,64 @@ async function initiateCookie() {
     The best thing this can be used for is name changes, as
     there are some mincor capitalization differences that
     eventually warrant this statement.
+    Values Include:
+
+    "BOSl-jdOSl-jlABABBENBd-AAAAgV___________" +
+    "____________________________________________________" +
+    "_______________________A"
+    (FULL CONSENT)
+
+    "BOSqLaKOSqLaKABABBENBdAAAAAgWAAA"
+    (NO CONSENT)
     __________________________________________________________
     */
     switch (domain) {
-        // placeholder statement
-        case "www.independent.co.uk":
-            valueSet = "BOSl-jdOSl-jlABABBENBd-AAAAgV___________" +
-            "____________________________________________________" +
-            "_______________________A";
+        // consented. This website just checks if there is a
+        // cookie called "banner-cookie". If one is present,
+        // it turns off the wall.
+        case "www.politico.eu":
+            nameSet = "banner-cookie";
+            valueSet = "0";
+            setCookie();
+            break;
+        // consented
+        case "www.theguardian.com":
+            nameSet = "GU_TK";
+            valueSet = "1.1534544181584";
+            domainSet = ".theguardian.com";
+            setCookie();
+            break;
+        // consented
+        case "www.bbc.com":
+            nameSet = "ckns_policy_exp";
+            valueSet = "1566096722751";
+            domainSet = ".bbc.com";
+            setCookie();
+            nameSet = "ckns_explicit";
+            valueSet = "1";
+            setCookie();
+            nameSet = "ckns_privacy";
+            valueSet = "1";
+            setCookie();
+            nameSet = "ckns_policy";
+            valueSet = "111";
+            setCookie();
+            break;
+        case "www.euronews.com":
+            /* This is based on an Opt-Out cookie that
+            quantserve provides. I can't seem to get
+            the necessary permissions to create the
+            cookie.*/
+            domainSet = ".quantserve.com";
+            nameSet = "qoo";
+            valueSet = "OPT-OUT";
+            setCookie();
+            break;
         default:
-            valueSet = "BOSl-jdOSl-jlABABBENBd-AAAAgV___________" +
-            "____________________________________________________" +
-            "_______________________A";
+            // cast a wide net
+            setCookie();
     }
     // console.log("Setting cookie value: " + valueSet.substr(0,16) + "....");
-    await browser.cookies.set({
-        url: urlSet,
-        name: nameSet,
-        value: valueSet,
-        httpOnly: httpOnlySet,
-        path: pathSet,
-        firstPartyDomain: firstPartyDomainSet,
-        storeId: storeIdSet,
-        domain: domainSet,
-    });
     // Temporary fix for window issue described below.
     main();
     broswer.webNavigation.onCreatedNavigationTarget.addListener(main);
@@ -67,4 +119,4 @@ function main() {
     browser.tabs.onActivated.addListener(initiateCookie);
     browser.webNavigation.onCommitted.addListener(initiateCookie);
 }
- main();
+main();
